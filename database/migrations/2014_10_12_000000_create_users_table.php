f1509de9a378a97d9d4fab214da5c4b018175418
woopyVolKno <nabilafajrindamayanti@gmail.com>
2023-10-12 19:52:09 +0700

    material icon

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,12 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // migration berfungsi untuk membantu kita membuat struktur database secara mudah
+        // unique agar kolom tersebut tidak bisa di isi data yang sama
+        // nullable agar kolom tersebut bisa di isi data kosong
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('fullname');
+            $table->string('avatar')->nullable();
+            $table->string('phone_number');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
