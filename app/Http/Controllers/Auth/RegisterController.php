75df862758afa74165c9336bd956e459c4429866
woopyVolKno <nabilafajrindamayanti@gmail.com>
2023-10-19 20:04:09 +0700

    feat: authertication

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    // membuat fauction index untuk menampilkan halaman register
+    public function index()
+    {
+        //return view berfungsi untuk menapilkan halaman register
+        return view('pages.auth.register');
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// membuat route untuk halaman register, ketika user mengakses /register
+// maka akan diarahkan ke RegisterController dengn method index
+Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index']);
